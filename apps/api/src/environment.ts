911c7177fca2e87b504787b93fea1c73b11cf17d
Ayush Abhishek Kumar <ayushabhishekkumar19@gmail.com>
2024-10-22 10:16:00 +0530

    Add dotenv and redis environment variables

--- apps/api/src/environment.ts
+++ apps/api/src/environment.ts
@@ -1,10 +1,13 @@
 import { z } from "zod";
 import { fromError } from "zod-validation-error";
+import "dotenv/config";
 
 const EnvironmentConfigurationSchema = z.object({
   NODE_ENV: z.enum(["development", "test", "production"]),
   PORT: z.coerce.number().positive().default(4000),
   JWT_SECRET: z.string(),
+  REDIS_ORDERBOOK_IP: z.string(),
+  REDIS_ORDERBOOK_PORT: z.coerce.number().min(1).max(65535),
 });
 
 export type EnvironmentConfiguration = z.infer<
