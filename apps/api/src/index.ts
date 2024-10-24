e392c19f67ba50390d83d1bd7adb1ce0eb2ea8a3
Ayush Abhishek Kumar <ayushabhishekkumar19@gmail.com>
2024-10-24 07:23:00 +0530

    connecting to redis server on server startup

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,9 +1,11 @@
 import { log } from "@repo/logger";
 import { createServer } from "./server";
 import { env } from "./environment";
+import { RedisClient } from "./clients/redis";
 
 const port = env.PORT;
 const server = createServer();
+RedisClient.getInstance();
 
 server.listen(port, () => {
   log(`api running on ${port} in ${env.NODE_ENV} mode`);
