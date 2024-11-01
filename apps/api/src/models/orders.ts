c4fa7a2165d5dcb9dc255541946d54dea9b01d37
Ayush Abhishek Kumar <ayushabhishekkumar19@gmail.com>
2024-11-01 14:17:00 +0530

    add schemas for getting open orders and on ramp requests

--- apps/api/src/models/orders.ts
+++ apps/api/src/models/orders.ts
@@ -7,12 +7,23 @@ export const CreateOrderRequestSchema = z.object({
   price: z.string(),
   userId: z.string(),
 });
-
 export type CreateOrderRequest = z.infer<typeof CreateOrderRequestSchema>;
 
 export const CancelOrderRequestSchema = z.object({
   orderId: z.string(),
   market: z.string(),
 });
-
 export type CancelOrderRequest = z.infer<typeof CancelOrderRequestSchema>;
+
+export const OnRampRequestSchema = z.object({
+  amount: z.string(),
+  userId: z.string(),
+  txnId: z.string(),
+});
+export type OnRampRequest = z.infer<typeof OnRampRequestSchema>;
+
+export const GetOpenOrdersRequestSchema = z.object({
+  userId: z.string(),
+  market: z.string(),
+});
+export type GetOpenOrdersRequest = z.infer<typeof GetOpenOrdersRequestSchema>;
