e71636a33c9cff163b6c0921fe71528397afc404
Ayush Abhishek Kumar <ayushabhishekkumar19@gmail.com>
2024-11-07 07:28:00 +0530

    Add routes for getting open orders and canelling orders

--- apps/api/src/controllers/orders.ts
+++ apps/api/src/controllers/orders.ts
@@ -1,8 +1,14 @@
 import type { Request, Response } from "express";
 import { fromError } from "zod-validation-error";
+import {
+  CANCEL_ORDER,
+  CancelOrderRequestSchema,
+  CREATE_ORDER,
+  CreateOrderRequestSchema,
+  GET_OPEN_ORDERS,
+  GetOpenOrdersRequestSchema,
+} from "@repo/models";
 import { RedisClient } from "../clients/redis";
-import { CREATE_ORDER } from "../types/constants";
-import { CreateOrderRequestSchema } from "../models/orders";
 
 export const createOrderController = (req: Request, res: Response): void => {
   (async () => {
@@ -28,5 +34,62 @@ export const createOrderController = (req: Request, res: Response): void => {
     }
   })().catch((err) => {
     return res.status(500).send(err);
+  });
+};
+
+export const cancelOrderController = (req: Request, res: Response): void => {
+  (async () => {
+    const parsedRequest = CancelOrderRequestSchema.safeParse(req.body);
+    if (!parsedRequest.success) {
+      const error = fromError(parsedRequest.error);
+
+      res.status(422).send(error.message);
+      return;
+    }
+
+    const requestData = parsedRequest.data;
+
+    try {
+      const response = await RedisClient.getInstance().sendAndAwait({
+        type: CANCEL_ORDER,
+        data: requestData,
+      });
+
+      return res.send(response);
+    } catch (err) {
+      return res.status(500).send(err);
+    }
+  })().catch((err) => {
+    res.status(500).send(err);
+  });
+};
+
+export const getOpenOrdersController = (req: Request, res: Response): void => {
+  (async () => {
+    const parsedRequest = GetOpenOrdersRequestSchema.safeParse({
+      userId: req.params.userId,
+      market: req.query.market,
+    });
+    if (!parsedRequest.success) {
+      const error = fromError(parsedRequest.error);
+
+      res.status(422).send(error.message);
+      return;
+    }
+
+    const requestData = parsedRequest.data;
+
+    try {
+      const response = await RedisClient.getInstance().sendAndAwait({
+        type: GET_OPEN_ORDERS,
+        data: requestData,
+      });
+
+      return res.send(response);
+    } catch (err) {
+      return res.status(500).send(err);
+    }
+  })().catch((err) => {
+    res.status(500).send(err);
   });
 };
--- apps/api/src/routes/orders.ts
+++ apps/api/src/routes/orders.ts
@@ -1,8 +1,14 @@
 import { Router } from "express";
-import { createOrderController } from "../controllers/orders";
+import {
+  cancelOrderController,
+  createOrderController,
+  getOpenOrdersController,
+} from "../controllers/orders";
 
 const OrderRouter: Router = Router();
 
+OrderRouter.get("/:userId", getOpenOrdersController);
 OrderRouter.post("/", createOrderController);
+OrderRouter.delete("/", cancelOrderController);
 
 export default OrderRouter;
