5c27b21a9cb50393e3fb18bc790080b75050e237
Ayush Abhishek Kumar <ayushabhishekkumar19@gmail.com>
2024-11-29 09:13:00 +0530

    feat: extend GetKlinesRequest schema with startTime, endTime, and interval fields

--- packages/models/src/orders.ts
+++ packages/models/src/orders.ts
@@ -7,6 +7,25 @@ export type GetTradesRequest = z.infer<typeof GetTradesRequestSchema>;
 
 export const GetKlinesRequestSchema = z.object({
   symbol: z.string(),
+  startTime: z.coerce.date(),
+  endTime: z.coerce.date().default(new Date()),
+  interval: z.enum([
+    "1m",
+    "3m",
+    "5m",
+    "15m",
+    "30m",
+    "1h",
+    "2h",
+    "4h",
+    "6h",
+    "8h",
+    "12h",
+    "1d",
+    "3d",
+    "1w",
+    "1M",
+  ]),
 });
 export type GetKlinesRequest = z.infer<typeof GetKlinesRequestSchema>;
 
