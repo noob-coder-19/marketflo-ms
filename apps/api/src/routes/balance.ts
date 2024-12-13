ab456629d3e737055ad7de0e44e7b11e3757bcec
Ayush Abhishek Kumar <ayushabhishekkumar19@gmail.com>
2024-12-13 23:47:00 +0530

    feat: add authentication middleware to balance routes

--- apps/api/src/routes/balance.ts
+++ apps/api/src/routes/balance.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
 import { getBalanceController, onRampController } from "../controllers/balance";
+import { verifyAuth } from "../middlewares/verify-auth";
 
 export const BalanceRouter: Router = Router();
 
-BalanceRouter.post("/", onRampController);
-BalanceRouter.get("/:userId", getBalanceController);
+BalanceRouter.post("/", verifyAuth, onRampController);
+BalanceRouter.get("/:userId", verifyAuth, getBalanceController);
