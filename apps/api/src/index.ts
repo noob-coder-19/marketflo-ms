c3dceaaeccb06b3f1b641cba193f1640aa867dce
Ayush Abhishek Kumar <ayushabhishekkumar19@gmail.com>
2024-12-22 04:44:00 +0530

    feat: implement SSL support for the API server

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,3 +1,5 @@
+import { readFileSync } from "node:fs";
+import https from "node:https";
 import { log } from "@repo/logger";
 import { createServer } from "./server";
 import { env } from "./environment";
@@ -6,11 +8,30 @@ import { UserRepository } from "./repositories/user";
 import { Mongo } from "./clients/mongo";
 
 const port = env.PORT;
+
+let options = {};
+if (env.SSL) {
+  if (!env.SSL_CERT_FILE || !env.SSL_KEY_FILE) {
+    throw new Error("SSL_CERT_FILE and SSL_KEY_FILE must be set");
+  }
+
+  options = {
+    key: readFileSync(env.SSL_KEY_FILE),
+    cert: readFileSync(env.SSL_CERT_FILE),
+  };
+}
+
 const server = createServer();
 RedisClient.getInstance();
 Mongo.getInstance();
 UserRepository.getInstance();
 
-server.listen(port, () => {
-  log(`api running on ${port} in ${env.NODE_ENV} mode`);
-});
+if (env.SSL) {
+  https.createServer(options, server).listen(port, () => {
+    log(`api running on ${port} in ${env.NODE_ENV} mode with SSL`);
+  });
+} else {
+  server.listen(port, () => {
+    log(`api running on ${port} in ${env.NODE_ENV} mode`);
+  });
+}
