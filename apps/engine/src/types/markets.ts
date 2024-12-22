f5051e30f3575fd7dd3d4429cac8c2955f301e8d
Ayush Abhishek Kumar <ayushabhishekkumar19@gmail.com>
2024-12-22 09:32:00 +0530

    feat: update supported quote assets to include NVB

--- apps/engine/src/types/markets.ts
+++ apps/engine/src/types/markets.ts
@@ -1,4 +1,4 @@
 import { z } from "zod";
 
-export const SUPPORTED_QUOTE_ASSETS = z.enum(["SOL"]);
+export const SUPPORTED_QUOTE_ASSETS = z.enum(["NVB"]);
 export type SupportedQuoteAsset = z.infer<typeof SUPPORTED_QUOTE_ASSETS>;
