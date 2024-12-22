232956c6fc282bb01be1ba36935dccc81ba0f32b
Ayush Abhishek Kumar <ayushabhishekkumar19@gmail.com>
2024-12-22 06:57:00 +0530

    feat: add SSL support to WebSocket server

--- apps/websocket/src/index.ts
+++ apps/websocket/src/index.ts
@@ -1,3 +1,6 @@
+import { readFileSync } from "node:fs";
+import https from "node:https";
+import http from "node:http";
 import { WebSocketServer } from "ws";
 import { log } from "@repo/logger";
 import { env } from "./environment";
@@ -8,11 +11,33 @@ SubscriptionManager.getInstance()
   .connectRedis()
   .then(() => {
     log("Connected to Redis");
-    const wss = new WebSocketServer({ port: env.PORT });
+
+    let options = {};
+    let server;
+    if (env.SSL) {
+      if (!env.SSL_CERT_FILE || !env.SSL_KEY_FILE) {
+        throw new Error("SSL_CERT_FILE and SSL_KEY_FILE must be set");
+      }
+
+      options = {
+        key: readFileSync(env.SSL_KEY_FILE),
+        cert: readFileSync(env.SSL_CERT_FILE),
+      };
+
+      server = https.createServer(options);
+    } else {
+      server = http.createServer();
+    }
+
+    const wss = new WebSocketServer({ server });
     wss.on("connection", (ws) => {
       log("Client connected");
 
       UserManager.getInstance().addUser(ws);
+    });
+
+    server.listen(env.PORT, () => {
+      log(`Server listening on port ${env.PORT}`);
     });
   })
   .catch((error) => {
